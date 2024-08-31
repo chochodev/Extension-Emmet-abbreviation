de00dee346f5e6900b11eb9a89f72b1e45b37197
Emmanuel Michael <michochoprogrammer@gmail.com>
2024-08-31 23:29:17 +0100

    untested: update

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,30 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
+// :::::::::::::::::::::: main function
 export function activate(context: vscode.ExtensionContext) {
+  console.log('Congratulations, your extension "custom-emmets" is now active!');
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "custom-emmets" is now active!');
+  let disposable = vscode.commands.registerCommand('custom-emmets.addAbbreviation', async () => {
+    // :::::::::::::::::: Prompt user for abbreviation and expansion
+    const abbreviation = await vscode.window.showInputBox({ prompt: 'Enter Emmet abbreviation' });
+    const expansion = await vscode.window.showInputBox({ prompt: 'Enter Emmet expansion' });
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('custom-emmets.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from custom-emmets!');
-	});
+    if (abbreviation && expansion) {
+      // Retrieve existing abbreviations
+      const config = vscode.workspace.getConfiguration('customEmmet');
+      let abbreviations = config.get<string[]>('abbreviations') || [];
 
-	context.subscriptions.push(disposable);
+      // Add new abbreviation
+      abbreviations.push(`${abbreviation}=${expansion}`);
+      await config.update('abbreviations', abbreviations, vscode.ConfigurationTarget.Global);
+
+      vscode.window.showInformationMessage(`Added Emmet abbreviation: ${abbreviation}`);
+    } else {
+      vscode.window.showErrorMessage('Abbreviation or expansion is missing.');
+    }
+  });
+
+  context.subscriptions.push(disposable);
 }
 
-// This method is called when your extension is deactivated
 export function deactivate() {}
